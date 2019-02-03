843bc7a3fcbfc4bcbc6e22112c3cc966bfd8ac9b
Junji Takakura <j.takakura@gmail.com>
2019-02-03 15:31:34 +0900

    2: Coin Dash - Part 4 – User Interface

--- Chapter 2 - Coin Dash/Main.gd
+++ Chapter 2 - Coin Dash/Main.gd
@@ -18,6 +18,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
 func spawn_coins():
 	for i in range(4 + level):
@@ -26,15 +28,35 @@ func spawn_coins():
 		c.screensize = screensize
 		c.position = Vector2(rand_range(0, screensize.x), rand_range(0, screensize.y))
 
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	for coin in $CoinContainer.get_children():
+		coin.queue_free()
+	$HUD.show_game_over()
+	$Player.die()
+
 func _ready():
 	randomize()
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	new_game()
 
 func _process(delta):
 	if playing and $CoinContainer.get_child_count() == 0:
 		level += 1
 		time_left += 5
 		spawn_coins()
+
+func _on_GameTimer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+func _on_Player_pickup():
+	score += 1
+	$HUD.update_score(score)
+
+func _on_Player_hurt():
+	game_over()
