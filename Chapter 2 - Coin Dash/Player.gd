7073184e2fca1e8bd232e566751df6e6b623eba4
Junji Takakura <j.takakura@gmail.com>
2019-02-03 12:48:52 +0900

    2: Coin Dash - Part 1 – Player scene

---
+++ Chapter 2 - Coin Dash/Player.gd
@@ -0,0 +1,51 @@
+extends Area2D
+
+export (int) var speed
+var velocity = Vector2()
+var screensize = Vector2(480, 720)
+
+signal pickup
+signal hurt
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+func start(pos):
+	set_process(true)
+	position = pos
+	$AnimatedSprite.animation = "idle"
+
+func die():
+	$AnimatedSprite.animation = "die"
+	set_process(false)
+
+func _process(delta):
+	get_input()
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
+	if velocity.length() > 0:
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.animation = "idle"
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("coins"):
+		area.pickup()
+		emit_signal("pickup")
+	if area.is_in_group("obstacles"):
+		emit_signal("hurt")
+		die()
